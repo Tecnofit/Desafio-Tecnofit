9a2112e3e55463a7d071301822b796e7301d4e65
helderferrari2 <helderferrari2@gmail.com>
2021-02-11 16:52:18 -0300

    Create initial Structure

--- tecnofit/database/migrations/2021_02_10_000000_create_trainings_table.php
+++ tecnofit/database/migrations/2021_02_10_000000_create_trainings_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTrainingsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,15 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('trainings', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['customer', 'instructor'])->default('customer');
+            $table->unsignedInteger('user_id');
+            $table->string('name', 100);
+            $table->boolean('active')->default(true);
             $table->timestamps();
+
+            /**Foreign Keys */
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
@@ -30,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('trainings');
     }
 }
---
+++ tecnofit/database/seeds/UsersTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'; // password
+
+        $items = [
+
+            /**Customer */
+            ['name' => 'John Doe', 'email' => 'customer@email.com', 'password' => $password, 'role' => 'customer', 'created_at' => now(), 'updated_at' => now()],
+
+            /**Instructor */
+            ['name' => 'Instructor', 'email' => 'instructor@email.com', 'password' => $password, 'role' => 'instructor', 'created_at' => now(), 'updated_at' => now()]
+        ];
+
+        User::insert($items);
+        factory(User::class, 10)->create();
+    }
+}
